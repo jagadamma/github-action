6b9c2c76d1757cd1aaa590d2828ae8d4d3d0a890
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-05-29 19:19:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL= 'mongodb+srv://abi:abi@cluster0.lrxjklz.mongodb.net/mongo?retryWrites=true&w=majority';
+const CONNECTION_URL= 'mongodb+srv://abilash:abilash@gcpcluster.rdas7ei.mongodb.net/?retryWrites=true&w=majority&appName=gcpcluster';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
