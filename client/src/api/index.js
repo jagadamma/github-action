d9929368845645db5b6fdc6ef08d909efd415fa8
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-05-30 00:29:40 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://35.224.40.127:5000';
+const url = 'http://35.224.40.127:5000/posts';
+
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
