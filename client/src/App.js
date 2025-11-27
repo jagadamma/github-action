50291cbcb66ff05b1ddf54a83105c142cf590d14
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-11-27 17:46:56 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">SUPER APP FROM THE CRAP</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">SUPER APP FROM THE CRAAAP</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
@@ -40,5 +40,6 @@ const App = () => {
 };
 
 export default App;
+
 
 
